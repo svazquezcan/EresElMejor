7fcd297a148ee7258db136e4a1eb334170617fb7
Alejandro <avgp@uoc.edu>
2020-11-15 20:34:58 +0100

    Añadir botón Inicio

--- App.js
+++ App.js
@@ -6,63 +6,98 @@ import { Evolucion } from './app/views/Evolucion.js';
 import { NuevoReto } from './app/views/NuevoReto.js';
 import { Contactar } from './app/views/Contactar.js';
 import { Perfil } from './app/views/Perfil.js';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
+const styles = StyleSheet.create({
+  boton: {
+    height: 30,
+    width: 60,
+    backgroundColor: '#0000ff',
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 5
+  },
+  textoBoton: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 14
+  }
+});
 const Stack = createStackNavigator();
-
 
 export default function App() {
   return (
     <NavigationContainer>
-     <Stack.Navigator 
-     initialRouteName="Inicio"
-     screenOptions={{ headerStyle: { 
-      backgroundColor: '#0984e3' 
-    },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      }
-   }}
-   >
+      <Stack.Navigator 
+        initialRouteName="Inicio"
+        screenOptions={{ headerStyle: { 
+          backgroundColor: '#0984e3' 
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        }
+      }}>
 
-
-       <Stack.Screen
-       name="Inicio"
-       component={Inicio}
-       options={{title: 'Inicio', 
-       headerShown: false}}
+      <Stack.Screen
+        name="Inicio"
+        component={Inicio}
+        options={{title: 'Inicio', 
+        headerShown: false}}
       />
 
-    <Stack.Screen
-       name='Evolucion'
-       component={Evolucion}
-       options={{
-         title: 'Evolucion',
-      }}
-      />
-
-    <Stack.Screen
-       name='NuevoReto'
-       component={NuevoReto}
-       options={{
-         title: 'NuevoReto',
+      <Stack.Screen
+        name='Evolucion'
+        component={Evolucion}
+        options={{
+          title: 'Evolución', 
+          headerRight: () => (
+            <TouchableOpacity style={styles.boton} onPress={() => this.props.navigate('Inicio')}>
+              <Text style={styles.textoBoton}>INICIO</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
-    <Stack.Screen
-       name='Contactar'
-       component={Contactar}
-       options={{
-         title: 'Contactar',
+
+      <Stack.Screen
+        name='NuevoReto'
+        component={NuevoReto}
+        options={{
+          title: 'Nuevo reto', 
+          headerRight: () => (
+            <TouchableOpacity style={styles.boton} onPress={() => this.props.navigate('Inicio')}>
+              <Text style={styles.textoBoton}>INICIO</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
-    <Stack.Screen
-       name='Perfil'
-       component={Perfil}
-       options={{
-         title: 'Perfil',
+
+      <Stack.Screen
+        name='Contactar'
+        component={Contactar}
+        options={{
+          title: 'Contactar', 
+          headerRight: () => (
+            <TouchableOpacity style={styles.boton} onPress={() => this.props.navigate('Inicio')}>
+              <Text style={styles.textoBoton}>INICIO</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
+
+      <Stack.Screen
+        name='Perfil'
+        component={Perfil}
+        options={{
+          title: 'Perfil', 
+          headerRight: () => (
+            <TouchableOpacity style={styles.boton} onPress={() => this.props.navigate('Inicio')}>
+              <Text style={styles.textoBoton}>INICIO</Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
